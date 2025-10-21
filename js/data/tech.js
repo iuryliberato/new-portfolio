552776b1d4cf8e0ea666d1adaca51b1a40d0365e
iuryliberato <iuryliberato@hotmail.com>
2025-10-21 13:51:08 +0200

    deploying

---
+++ js/data/tech.js
@@ -0,0 +1,19 @@
+export const TECH = [
+    { name: "HTML",       icon: "../../public/tags-images/html-5.svg" },
+    { name: "CSS3",       icon: "../../public/tags-images/css3.svg" },
+    { name: "JavaScript", icon: "../../public/tags-images/javaScript.svg" },
+    { name: "React",      icon: "../../public/tags-images/react.svg" },
+    { name: "Node.js",    icon: "../../public/tags-images/node-js.svg" },
+    { name: "Express",    icon: "../../public/tags-images/node-js.svg" }, // swap to express.svg if you have it
+    { name: "MongoDB",    icon: "../../public/tags-images/mongodb.svg" },
+    { name: "Mongoose",   icon: "../../public/tags-images/mongodb.svg" },
+    { name: "NPM",        icon: "../../public/tags-images/npm.svg" },
+    { name: "Yarn",       icon: "../../public/tags-images/yarn.svg" },
+    { name: "Python",     icon: "../../public/tags-images/python.svg" },
+    { name: "Django",     icon: "../../public/tags-images/django.svg" },
+    { name: "Bootstrap",  icon: "../../public/tags-images/bootstrap.svg" },
+    { name: "SASS",       icon: "../../public/tags-images/sass.svg" },
+    { name: "Figma",      icon: "../../public/tags-images/figma.svg" },
+    { name: "GitHub",     icon: "../../public/tags-images/github.svg" },
+  ];
+  
