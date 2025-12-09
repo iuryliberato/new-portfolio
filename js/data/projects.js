8e248e2e145da59fe8262f0f5755b7944b068a7a
iuryliberato <iuryliberato@hotmail.com>
2025-12-09 14:50:42 +0100

    added applio to my portfolio

--- js/data/projects.js
+++ js/data/projects.js
@@ -1,7 +1,35 @@
 export const PROJECTS = [
+  {
+    title: "Applio/ Job Application Tracker",
+    desc: "I created Applio, a full-stack job-application manager with AI integration. It offers real-time CV and Cover Letter generation, job-ad scraping, auto-filled user profiles, progress statuses, notes, and secure authentication.",
+    video: "../../public/videos/ApplioVideo.mp4",
+    live: "https://applio-job-tracker-025135847b67.herokuapp.com/",
+    git: "https://github.com/iuryliberato/applywise-frontend",
+    tags: [
+      "JavaScript (ESNext)",
+      "React",
+      "Vite",
+      "CSS3",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "JWT Authentication",
+      "OpenAI API",
+    ],
+    buttons: { liveStyle: "accent", gitStyle: "outline" }
+  },
+  {
+    title: "Patrycja Langa Personal Website",
+    desc: "I've designed and built a dual-language personal website using Next.js, React, JavaScript and Styled-Components. The website presents education, skills and services, and includes a contact form powered by EmailJS.",
+    video: "../../public/videos/PatriciaVideo.mp4",
+    live: "https://www.patrycjalanga.com/En",
+    git: "https://github.com/iuryliberato/PatriciaWebsite",
+    tags: ["HTML5", "Styled Components", "JavaScript", "React", "Next.js", "Yarn"],
+    buttons: { liveStyle: "accent", gitStyle: "outline" }
+  },
     {
-      title: "Get'a Wave",
-      desc: "I built a grid-based Frogger-style game in one week using JavaScript, HTML and CSS. The game features three characters, keyboard navigation, obstacles and progressive difficulty.",
+      title: "Get'a Wave Growser Game",
+      desc: "I built a browser-based crossing-game called “Get a Wave” in a weekend using HTML, CSS and JavaScript. The game features three surfers, keyboard navigation (arrow keys), hazards like speeding cars, trucks and seagulls — plus a river to avoid — and challenges you to guide all surfers safely to the beach.",
       video: "../../public/videos/GetaWaveVideo.mp4",
       live: "https://iuryliberato.github.io/Get-a-wave/",
       git: "https://github.com/iuryliberato/Get-a-wave",
@@ -17,14 +45,6 @@ export const PROJECTS = [
       tags: ["HTML5", "React", "Python", "Django", "Styled Components", "PostgreSQL"],
       buttons: { liveStyle: "accent", gitStyle: "outline" }
     },
-    {
-      title: "Patrycja Langa Personal Website",
-      desc: "I've designed and built a dual-language personal website using Next.js, React, JavaScript and Styled-Components. The website presents education, skills and services, and includes a contact form powered by EmailJS.",
-      video: "../../public/videos/PatriciaVideo.mp4",
-      live: "https://www.patrycjalanga.com/En",
-      git: "https://github.com/iuryliberato/PatriciaWebsite",
-      tags: ["HTML5", "Styled Components", "JavaScript", "React", "Next.js", "Yarn"],
-      buttons: { liveStyle: "accent", gitStyle: "outline" }
-    }
+   
   ];
   
